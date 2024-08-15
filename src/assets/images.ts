7edb80428fa0e0cd8e9102ed0f1a185c5b03fec5
xue2227 <ya20020222@gmail.com>
2024-08-15 22:02:50 +0800

    first commit

---
+++ src/assets/images.ts
@@ -0,0 +1,86 @@
+import img1 from "./photos/1.png";
+import img2 from "./photos/2.png";
+import img3 from "./photos/3.png";
+import img4 from "./photos/4.png";
+import img5 from "./photos/5.png";
+import img6 from "./photos/6.png";
+import img7 from "./photos/7.png";
+import img8 from "./photos/8.png";
+import img9 from "./photos/9.png";
+import img10 from "./photos/10.png";
+import img11 from "./photos/11.png";
+import img12 from "./photos/12.png";
+import img13 from "./photos/13.png";
+import img14 from "./photos/14.png";
+import img15 from "./photos/15.png";
+import img16 from "./photos/16.png";
+import img17 from "./photos/17.png";
+import img18 from "./photos/18.png";
+import img19 from "./photos/19.png";
+import img20 from "./photos/20.png";
+
+import img21 from "./photos/21.png";
+import img22 from "./photos/22.png";
+import img23 from "./photos/23.png";
+import img24 from "./photos/24.png";
+import img25 from "./photos/25.png";
+import img26 from "./photos/26.png";
+import img27 from "./photos/27.png";
+import img28 from "./photos/28.png";
+import img29 from "./photos/29.png";
+import img30 from "./photos/30.png";
+import img31 from "./photos/31.png";
+import img32 from "./photos/32.png";
+import img33 from "./photos/33.png";
+import img34 from "./photos/34.png";
+import img35 from "./photos/35.png";
+import img36 from "./photos/36.png";
+import img37 from "./photos/37.png";
+import img38 from "./photos/38.png";
+import img39 from "./photos/39.png";
+import img40 from "./photos/40.png";
+
+const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
+  img26,
+  img27,
+  img28,
+  img29,
+  img30,
+  img31,
+  img32,
+  img33,
+  img34,
+  img35,
+  img36,
+  img37,
+  img38,
+  img39,
+  img40
+];
+
+export default images;
---
+++ src/components/GifImage.vue
@@ -0,0 +1,27 @@
+<template>
+    <img :src="src" :alt="alt" :class="class" />
+  </template>
+  
+  <script>
+  export default {
+    name: 'GifImage',
+    props: {
+      src: {
+        type: String,
+        required: true
+      },
+      alt: {
+        type: String,
+        default: ''
+      },
+      class: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* 你可以在這裡添加任何特定於這個組件的樣式 */
+  </style>
