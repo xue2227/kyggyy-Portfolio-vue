fe56d25cbf4d5fa4133d004c436a949f7267b283
xue2227 <ya20020222@gmail.com>
2024-08-22 04:09:17 +0800

    Loading animation Update
    Finish all Loading Animation

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -4,10 +4,12 @@
     </div>
     <div class="absolute inset-0 bg-black z-40" ref="black"></div>
     <div class="absolute inset-0 bg-white z-10" ref="white2"></div>
-    <div class="absolute inset-0 bg-white z-10 flex justify-center items-center text-center leading-[ " ref="white3">
-        <div class="font-semibold overflow-hidden [font-size:3rem] flex ">
-            <div ref="welcomeText" class="leading-[0.7] pt-6">Welcom ,</div>
-            <div ref="restText" class="leading-[0.7] pt-6">&nbsp;to the wonder of 3D</div>
+    <div class="absolute inset-0 z-10 flex justify-center items-center text-center overflow-hidden" ref="white3">
+        <div class="absolute top-0 left-0 bottom-0 bg-white z-20 w-1/2" ref="leftDoor"></div>
+        <div class="absolute top-0 right-0 bottom-0 bg-white z-20 w-1/2" ref="rightDoor"></div>
+        <div class="font-semibold overflow-hidden [font-size:3rem] flex z-30">
+            <div ref="welcomeText" class="leading-[0.7] pt-6">Welcome</div>
+            <div ref="restText" class="leading-[0.7] pt-6">&nbspto the wonder of 3D</div>
         </div>
     </div>
 
@@ -23,6 +25,8 @@ const counter = ref(null);
 const black = ref(null);
 const white2 = ref(null);
 const white3 = ref(null);
+const leftDoor = ref(null);
+const rightDoor = ref(null);
 const welcomeText = ref(null);
 const restText = ref(null);
 
@@ -53,7 +57,7 @@ onMounted(() => {
     // 數字從 0 到 100
     tl.to(counter.value, {
         delay: 0.2,
-        duration: 1.8,
+        duration: 1,
         innerText: 100,
         snap: { innerText: 1 },
         ease: CustomEase.create("custom", "M0,0 C0,0 -0.01,-0.021 0.11,0.026 0.423,0.155 0.373,0.636 0.417,0.656 0.487,0.687 0.67,0.677 0.716,0.697 0.933,0.788 1,1 1,1 "), onUpdate: function () {
@@ -71,31 +75,47 @@ onMounted(() => {
     },);
 
     tl.from(welcomeText.value.querySelectorAll('div'), {
-        delay: -0.45,
         y: 100,
         duration: 0.5,
         ease: "back.out(1)",
-        stagger: 0.025
-    },);
+        stagger: 0.04
+    },"-=0.5");
 
     tl.from(restText.value.querySelectorAll('div'), {
-        delay: 0.1,
         y: 100,
         duration: 0.5,
         ease: "back.out(1)",
-        stagger: 0.025
-    });
+        stagger: 0.04
+    },"-=0.5" );
+    
+    //字消失
+    tl.to([...welcomeText.value.querySelectorAll('div'), ...restText.value.querySelectorAll('div')], {
+        delay: 0.5,
+        opacity: 0,
+        duration: 0.3,
+        ease: "power1.out",
+    },);
 
  
-    tl.to([white3.value], {
-        delay: 0.5,
+    // 開門
+    tl.to(leftDoor.value, {
+        delay: 0.25,
         duration: 1,
-        y: "-100vh",
-        ease: "power4.inOut",
-    });
+        x: '-100%',
+        ease: "power3.inOut"
+    }, );
+
+    tl.to(rightDoor.value, {
+        delay: 0.25,
+        duration: 1,
+        x: '100%',
+        ease: "power3.inOut"
+    }, "-=1.25");
 
 
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
